890c5fdd861e2a7956c69e9a47a13b69e306d89a
Murad501 <mm845635@gmail.com>
2022-11-08 02:02:38 +0600

    home page done

--- src/Components/Shared/ServiceCard/ServiceCard.js
+++ src/Components/Shared/ServiceCard/ServiceCard.js
@@ -4,16 +4,16 @@ const ServiceCard = ({ service }) => {
     const {picture, title, price, description} = service
     console.log(service)
   return (
-    <div className="card border">
+    <div className="card border p-2 rounded-sm hover:shadow-lg">
       <figure>
         <img src={picture} alt="Service_Image" />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p>{description.length > 100 ? description.slice(0, 100) + '...' : description}</p>
-        <div className="card-actions justify-end">
-            <p>Price: ${price}</p>
-          <button className="btn btn-primary">Buy Now</button>
+      <div>
+        <h2 className="mt-5 font-bold text-2xl">{title}</h2>
+        <p className="my-5">{description.length > 100 ? description.slice(0, 100) + '...' : description}</p>
+        <div className="card-actions justify-between items-center">
+            <p className="text-xl font-semibold">Price: <span className="text-success">${price}</span></p>
+          <button className="btn btn-success text-white font-semibold">Details</button>
         </div>
       </div>
     </div>
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import ServiceCard from '../../../Components/Shared/ServiceCard/ServiceCard';
-import Carousel from '../Carousel/Carousel';
-import NewsLetter from '../NewsLetter/NewsLetter';
+import React, { useEffect, useState } from "react";
+import ServiceCard from "../../../Components/Shared/ServiceCard/ServiceCard";
+import Carousel from "../Carousel/Carousel";
+import NewsLetter from "../NewsLetter/NewsLetter";
 
 const Home = () => {
-    const [services, setServices] = useState([])
+  const [services, setServices] = useState([]);
 
-    useEffect(()=>{
-        fetch('http://localhost:5000/home')
-        .then(res => res.json())
-        .then(data => setServices(data))
-    },[])
-    return (
-        <div>
-            <Carousel></Carousel>
-            <div>
-                {
-                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
-                }
-            </div>
-            <NewsLetter></NewsLetter>
+  useEffect(() => {
+    fetch("http://localhost:5000/home")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <Carousel></Carousel>
+      <div className="my-20">
+      <h1 className="text-2xl lg:text-5xl font-bold mb-10 text-center">Our Services</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {services.map((service) => (
+            <ServiceCard key={service._id} service={service}></ServiceCard>
+          ))}
         </div>
-    );
+        <div className="flex justify-center">
+        <button className="btn btn-success w-40 rounded-none text-lg mt-10 text-white font-semibold">
+          See All 
+        </button>
+        </div>
+      </div>
+      <NewsLetter></NewsLetter>
+    </div>
+  );
 };
 
 export default Home;
