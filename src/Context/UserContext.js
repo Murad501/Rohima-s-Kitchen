01a36f0eeeda1701493f0232a1ef60686a2fe8ce
Murad501 <mm845635@gmail.com>
2022-11-07 22:46:14 +0600

    user context setup done

---
+++ src/Context/UserContext.js
@@ -0,0 +1,61 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../Firebase/Firebase.config';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+
+const auth = getAuth(app)
+
+const authContext = createContext()
+
+const UserContext = ({children}) => {
+
+    const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    const googleProvider = new GoogleAuthProvider()
+
+    const createUserByEmail = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInUserByEmail = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleLogIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        return ()=> unsubscribe()
+    },[])
+
+    const authInfo = {
+        user, 
+        loading,
+        createUserByEmail,
+        signInUserByEmail,
+        googleLogIn,
+        logOut
+    }
+
+    return (
+        <authContext.Provider value={authInfo}>
+            {children}
+        </authContext.Provider>
+    );
+};
+
+export default UserContext;
