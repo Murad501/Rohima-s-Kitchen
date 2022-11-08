58a19777491cc6ed5cc2b930735524bcbfec7ca5
Murad501 <mm845635@gmail.com>
2022-11-08 23:38:39 +0600

    react photoview item completed

--- src/Components/Shared/ServiceCard/ServiceCard.js
+++ src/Components/Shared/ServiceCard/ServiceCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const ServiceCard = ({ service }) => {
   const { picture, title, price, description, _id } = service;
@@ -7,7 +9,11 @@ const ServiceCard = ({ service }) => {
   return (
     <div className="card border p-2 rounded-sm hover:shadow-lg">
       <figure>
-        <img src={picture} alt="Service_Image" />
+        <PhotoProvider>
+        <PhotoView src={picture}>
+        <img src={picture} style={{ objectFit: 'cover' }} alt="Service_Image" />
+        </PhotoView>
+        </PhotoProvider>
       </figure>
       <div>
         <h2 className="mt-5 font-bold text-2xl">{title}</h2>
